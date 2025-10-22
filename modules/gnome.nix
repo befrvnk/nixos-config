58dd2bffd45268a06d17c3335da80d3ac8bc0573
Frank Hermann <hermann.frank@gmail.com>
2025-10-22 14:16:04 +0200

    Enable experimental display scaling on GNOME

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -3,7 +3,17 @@
 {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome = {
+    enable = true;
+    # This section enables the fractional scaling options (125%, 150%, etc.) 
+    # in the GNOME Settings -> Displays menu.
+    extraGSettingsOverrides = ''
+      [org.gnome.mutter]
+      experimental-features=['scale-monitor-framebuffer']
+    '';
+    # Ensure Mutter is available to provide the GSettings schema
+    extraGSettingsOverridePackages = [ pkgs.mutter ];
+  };
   services.xserver.xkb = {
     layout = "us";
     variant = "";
