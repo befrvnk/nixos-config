2cd37548078ed4922c5f811293b41d038c3f19bd
Frank Hermann <hermann.frank@gmail.com>
2025-11-06 21:39:59 +0100

    Extract stylix

--- home-manager/frank.nix
+++ home-manager/frank.nix
@@ -9,68 +9,9 @@
 }:
 
 {
-  # Base stylix configuration (default to dark theme)
-  stylix = {
-    enable = true;
-    autoEnable = true;
-    polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    image = ./wallpapers/mountain.jpg;
-
-    fonts = {
-      serif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Serif";
-      };
-      sansSerif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Sans";
-      };
-      monospace = {
-        package = pkgs.nerd-fonts.fira-code;
-        name = "FiraCode Nerd Font";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-color-emoji;
-        name = "Noto Color Emoji";
-      };
-      sizes = {
-        applications = 11;
-        terminal = 11;
-        desktop = 11;
-      };
-    };
-    cursor = {
-      package = pkgs.quintom-cursor-theme;
-      name = "Quintom_Snow";
-      size = 24;
-    };
-  };
-
-  specialisation =
-    let
-      # Import shared theme definitions
-      themes = import ./themes.nix { inherit pkgs; };
-    in
-    {
-      dark.configuration = {
-        stylix = {
-          polarity = pkgs.lib.mkForce "dark";
-          base16Scheme = pkgs.lib.mkForce themes.dark.scheme;
-          image = pkgs.lib.mkForce themes.dark.wallpaper;
-        };
-      };
-      light.configuration = {
-        stylix = {
-          polarity = pkgs.lib.mkForce "light";
-          base16Scheme = pkgs.lib.mkForce themes.light.scheme;
-          image = pkgs.lib.mkForce themes.light.wallpaper;
-        };
-      };
-    };
-
   imports = [
     zen-browser.homeModules.beta
+    ./stylix.nix
     (import ./zed.nix { inherit pkgs pkgs-unstable; })
     (import ./ghostty.nix {
       inherit config pkgs pkgs-unstable;
