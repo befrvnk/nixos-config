7dc6e502fbe6c2844db78cf485384d19861aade3
Frank Hermann <hermann.frank@gmail.com>
2025-12-10 10:14:56 +0100

    Fix built-in microphone not working on Framework 13 AMD AI 300
    Disable UCM profiles in WirePlumber to fix DMIC microphone being
    inoperational. UCM selects "Play HiFi quality Music" profile which
    disables the built-in microphone on recent PipeWire/kernel versions.
    
    Workaround from: https://github.com/NixOS/nixos-hardware/issues/1603

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -8,5 +8,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    # Disable UCM profiles to fix built-in microphone on Framework 13 AMD AI 300
+    # UCM selects "Play HiFi quality Music" profile which disables DMIC
+    # See: https://github.com/NixOS/nixos-hardware/issues/1603
+    wireplumber.extraConfig.no-ucm = {
+      "monitor.alsa.properties" = {
+        "alsa.use-ucm" = false;
+      };
+    };
   };
 }
