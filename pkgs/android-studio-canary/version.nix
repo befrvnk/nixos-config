752dcedea82696425b63fbf519969fcf18872175
Frank Hermann <hermann.frank@gmail.com>
2026-03-14 18:29:55 +0100

    chore: flake-update

--- pkgs/android-studio-canary/version.nix
+++ pkgs/android-studio-canary/version.nix
@@ -1,7 +1,7 @@
 # Android Studio Canary version info
 # Updated automatically by: ./scripts/update-android-studio-canary.sh
 {
-  version = "2025.3.3.3";
-  hash = "sha256-An9lHZ0TqcXGskJG71jUs0fSdKjoKXPWIT9QjLXSASc=";
-  url = "https://dl.google.com/dl/android/studio/ide-zips/2025.3.3.3/android-studio-panda3-canary3-linux.tar.gz";
+  version = "2025.3.3.4";
+  hash = "sha256-HzN1ya6Lqn/F4+j+n+duIRzACcpSBeFK4yN9bx9CYjM=";
+  url = "https://dl.google.com/dl/android/studio/ide-zips/2025.3.3.4/android-studio-panda3-canary4-linux.tar.gz";
 }
