670e3165268317b3158d6d6f78a78164bcaa3c8d
Frank Hermann <hermann.frank@gmail.com>
2026-04-14 13:58:23 +0200

    chore: flake-update

--- pkgs/pi-coding-agent/package.nix
+++ pkgs/pi-coding-agent/package.nix
@@ -7,23 +7,23 @@
 }:
 
 let
-  version = "0.66.1";
+  version = "0.67.1";
   sources = {
     x86_64-linux = {
       url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-x64.tar.gz";
-      hash = "sha256:fed71baa4ff3f97fc090ce339ee7bc990fc62f8ee3ea4f32b234c7f167e20c30";
+      hash = "sha256:21d71fd822f2fc000330dbda17eaaac507570ce8e45d19eac154dc1cb996eec8";
     };
     aarch64-linux = {
       url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-arm64.tar.gz";
-      hash = "sha256:64dfed93265d69e10e67c7852a1f3d658ab94a228ceffa9603211daa71d1a385";
+      hash = "sha256:b2d659b974501aa79865c8a9d150a1f4c307bd5699b468dfba4e84f35aa92ed1";
     };
     x86_64-darwin = {
       url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-darwin-x64.tar.gz";
-      hash = "sha256:932b7df46c3a4be473059796a99d31458baccd17bc9a56a1b2e3ca77ddeb8d90";
+      hash = "sha256:53ab9ab6872df1ac2f77cb9b2ceeb66ff245af51a6eb65903301691c76e61edf";
     };
     aarch64-darwin = {
       url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-darwin-arm64.tar.gz";
-      hash = "sha256:fe8f1b18465797c9c0f6b7bb097cf22bbad5e388a1dec36b557b497352fd4e5a";
+      hash = "sha256:1412b3d330178981fbd7e8cca1e6b34f0337b4b25617d7134291c7328efe52c7";
     };
   };
   source =
