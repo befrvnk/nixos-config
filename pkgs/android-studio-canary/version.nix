4229c250fd71c2b255c3a6b1c241dfda01a6e41a
Frank Hermann <hermann.frank@gmail.com>
2026-06-09 10:17:20 +0200

    chore: flake-update

--- pkgs/android-studio-canary/version.nix
+++ pkgs/android-studio-canary/version.nix
@@ -1,7 +1,7 @@
 # Android Studio Canary version info
 # Updated automatically by: ./scripts/update-android-studio-canary.sh
 {
-  version = "2026.1.2.4";
-  hash = "sha256-fnJYHZPy9bOZJ2leG2+Mr5JGH5HMc2HeMeYGHBUxJXo=";
-  url = "https://dl.google.com/dl/android/studio/ide-zips/2026.1.2.4/android-studio-quail2-canary4-linux.tar.gz";
+  version = "2026.1.2.6";
+  hash = "sha256-mXYO7Ptws4lGXRJsdgaFkX8Kob5mQFJgGM3xNeMBFjA=";
+  url = "https://dl.google.com/dl/android/studio/ide-zips/2026.1.2.6/android-studio-quail2-canary6-linux.tar.gz";
 }
--- pkgs/pi-coding-agent/package.nix
+++ pkgs/pi-coding-agent/package.nix
@@ -7,23 +7,23 @@
 }:
 
 let
-  version = "0.78.0";
+  version = "0.79.0";
   sources = {
     x86_64-linux = {
       url = "https://github.com/earendil-works/pi/releases/download/v${version}/pi-linux-x64.tar.gz";
-      hash = "sha256:8ac03343d1e1228106e8172157f32d6b882829e46b34feaf577f171a5f1387cc";
+      hash = "sha256:0c92e73a0cd8c0d4060e29dbe677071917461703edd2c0e81ea80c14c32a0daa";
     };
     aarch64-linux = {
       url = "https://github.com/earendil-works/pi/releases/download/v${version}/pi-linux-arm64.tar.gz";
-      hash = "sha256:49155173682473720d9decf4deecbed754fae84925ef003c0b66aac31d5f9005";
+      hash = "sha256:92a9fab642847f7d22bb5014b5f40a7165d7acd6e1e504e47ca14242bc4885b8";
     };
     x86_64-darwin = {
       url = "https://github.com/earendil-works/pi/releases/download/v${version}/pi-darwin-x64.tar.gz";
-      hash = "sha256:66074b271260068199f47738a172397f1e0b5a3334697dd2acea35bbd3470b1c";
+      hash = "sha256:89d6225488c855bfeacd8f2fe8645b10defd91aeabf7f04273ee950668e8e2ef";
     };
     aarch64-darwin = {
       url = "https://github.com/earendil-works/pi/releases/download/v${version}/pi-darwin-arm64.tar.gz";
-      hash = "sha256:68ebbe4f56a136a1c7bace3393eca4ad0aa1fd9f253b797fd370058bd39fe070";
+      hash = "sha256:7d8ea65dbbf4e1ef0f309fb17e21780c6d61e20334c66135e5513ceac230584a";
     };
   };
   source =
