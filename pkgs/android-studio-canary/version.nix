2e3e14515466935b2e4ca9623aacaa81c10ec3ed
Frank Hermann <hermann.frank@gmail.com>
2026-09-02 09:45:37 +0200

    chore: flake-update

--- pkgs/android-studio-canary/version.nix
+++ pkgs/android-studio-canary/version.nix
@@ -1,7 +1,7 @@
 # Android Studio Canary version info
 # Updated automatically by: ./scripts/update-android-studio-canary.sh
 {
-  version = "2026.2.1.2";
-  hash = "sha256-iqRRIcH6OC65TVGsQ6TouWZk7t0hMh7VQTyWVAcCUMM=";
-  url = "https://dl.google.com/dl/android/studio/ide-zips/2026.2.1.2/android-studio-rabbit1-canary2-linux.tar.gz";
+  version = "2026.2.1.3";
+  hash = "sha256-L44Xltg4bqLz3Vpun4rTBnh9HPinWm0rsy4d8U1ZPQE=";
+  url = "https://dl.google.com/dl/android/studio/ide-zips/2026.2.1.3/android-studio-rabbit1-canary3-linux.tar.gz";
 }
